0531c639795480b983fa823cb4d33895c2623647
Michael Siebert <siebertm85@googlemail.com>
2015-02-19 11:38:37 +0100

    Add support for rails 4.0 (load_fixtures takes no arguments until 4.1) and minitest-spec

--- lib/transactionata.rb
+++ lib/transactionata.rb
@@ -5,35 +5,44 @@ module Transactionata
   # without the speed drop.
   #
   # Please note that you'll have to set up empty fixture files (and load them, see
-  # the list of fixtures above) in order to clean up the database before you launch 
+  # the list of fixtures above) in order to clean up the database before you launch
   # your tests.
   #
   # Usage: In your test class, do:
   #   test_data do
   #     Factory.create(:foobar)
   #     # etc...
-  #     
+  #
   #     # or for Machinist
   #     @person =  Person.make
   #   end
   #
-  # The foobar and @person record will be available in all your tests and rolls back 
+  # The foobar and @person record will be available in all your tests and rolls back
   # even if you modify it in your test cases thanks to transactions.
   #
   def test_data(&blk)
     self.class_eval do
       class << self
-        attr_accessor :test_data_block
       end
+
+      class_attribute :_test_data_block
+      class_attribute :_test_data_vars
+
       setup :load_test_data_vars
 
       alias_method :original_load_fixtures, :load_fixtures
-      def load_fixtures(config)
+      def load_fixtures(*config)
         # We need to return the value of the original load_fixtures method, so that all the fixtures magic works
-        hash = original_load_fixtures(config)
+        hash = original_load_fixtures(*config)
         existing_klass_instance_vars = self.class.instance_variables
-        self.class.test_data_block.call
-        @@test_data_vars = (self.class.instance_variables - existing_klass_instance_vars)
+        self.class._test_data_block.call
+        self.class._test_data_vars ||= {}
+
+        vars = (self.class.instance_variables - existing_klass_instance_vars)
+
+        vars.each do |var|
+          self.class._test_data_vars[var] = self.class.instance_variable_get(var)
+        end
 
         if defined?(ActiveRecord::FixtureSet) # Rails 4
           ActiveRecord::FixtureSet.reset_cache
@@ -46,13 +55,13 @@ module Transactionata
       end
 
       def load_test_data_vars
-        @@test_data_vars.each do |new_ivar| # Added block
-          self.instance_variable_set(new_ivar, self.class.instance_variable_get(new_ivar))
+        self.class._test_data_vars.each do |(new_ivar, value)| # Added block
+          self.instance_variable_set(new_ivar, value)
         end
       end
-
     end
-    self.test_data_block = blk
+
+    self._test_data_block = blk
   end
 end
 
